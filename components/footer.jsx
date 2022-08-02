62c3e8b34f2cb278da0de6978ad24809c60f3fd4
Jonas Lidbom <lidbomj@gmail.com>
2022-08-02 14:26:39 +0200

    Changes to footer

--- components/footer.jsx
+++ components/footer.jsx
@@ -4,16 +4,18 @@ export default function Footer() {
   return (
     <>
     <footer>
+      <div class="footer-logo">
         <Image
           src="/Vaca-logo.svg"
           height={40}
           width={100}
           class="image"
         />
+      </div>
       <div class="footer-links">
         <a>Privacy Policy</a>
         <a>Privacy Policy</a>
-        <a>Privacy Policy</a>
+        <a id="last-link">Privacy Policy</a>
       </div>
     </footer>
     </>
